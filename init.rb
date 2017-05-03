f99d587ebe72d0de7d113f0ceb997fad33f370d5
Darcy70 <darcy0721@gmail.com>
2017-05-03 11:51:58 +0800

    application paths

---
+++ init.rb
@@ -0,0 +1,13 @@
+#### Food Finder ####
+#
+# Launch this Ruby file from the command line to get started.
+#
+
+App_ROOT = File.dirname(__FILE__)
+
+# require "#{App_ROOT}/lib/guide"
+# require File.join(App_ROOT, 'lib', 'guide.rb')
+# require File.join(App_ROOT, 'lib', 'guide')
+
+$:.unshift(File.join(App_ROOT, 'lib'))
+require 'guide'
